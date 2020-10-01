d674c808b10e7f7ce54d41e2e92647793be220ce
Zach Drummond <zachdrummond3@gmail.com>
2020-10-01 18:42:34 -0400

    Added routes

--- server.js
+++ server.js
@@ -7,10 +7,30 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(_dirname, "./public/notes.html"));
+});
+
+app.get("*", function(req, res){
+  res.sendFile(path.join(_dirname, "./index.html"));
+});
+
+app.get("/api/notes", function(req, res){
+  res.readFile(path.join(_dirname, "db.json"));
+});
+
+app.post("/api/notes", function(res, req){
+
+});
+
+app.delete("/api/notes/:id", function(res, req){
+
 });
